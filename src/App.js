21e63ff768a067681b6ae8d1f77a930668341fca
Erlesand <Erlesand@users.noreply.github.com>
2020-10-04 15:41:29 +0200

    Implemented CSS styling of application

--- src/App.js
+++ src/App.js
@@ -1,54 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <section className="todo-app">
-        <header className="todo-header">
-          <h1>Todo</h1>
+    <div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="todo-header">
+        <h1>Todo</h1>
+      </div>
 
-          <input type="text" name="new-item" />
-        </header>
+      <section className="todo-app">
+        <input
+          type="text"
+          className="todo-app-add"
+          placeholder="What shall we do today?"
+        />
 
         <ul id="items">
           <li className="todo-item">
             <div className="todo-item-view">
               <input type="checkbox" className="todo-item-toggle" />
+              <label>Dip your toes in the JavaScript-water</label>
+              <button className="todo-item-destroy">×</button>
+            </div>
+
+            <div className="todo-item-edit">
               <input
                 className="todo-item-edit"
                 value="Dip your toes in the JavaScript-water"
               />
-              <label></label>
-              <button className="todo-item-destroy">X</button>
             </div>
           </li>
 
           <li className="todo-item">
             <div className="todo-item-view">
               <input type="checkbox" className="todo-item-toggle" />
+              <label>Build an awesome todo-app!</label>
+              <button className="todo-item-destroy">×</button>
+            </div>
 
+            <div className="todo-item-edit">
               <input
                 className="todo-item-edit"
-                value="Build an awesome todo-app"
+                value="Build an awesome todo-app!"
               />
-              <label></label>
-              <button className="todo-item-destroy">X</button>
             </div>
           </li>
         </ul>
 
-        <div>
+        <div className="todo-list-summary">
           <div>2 items left</div>
-          <div>
+          <div className="todo-list-filters">
             <input type="radio" name="filter" /> All
             <input type="radio" name="filter" /> Active
             <input type="radio" name="filter" /> Completed
           </div>
 
           <div>
-            <button>Clear completed</button>
+            <button className="todo-list-clear">Clear completed</button>
           </div>
         </div>
       </section>
