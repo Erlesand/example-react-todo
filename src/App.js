50059d322f82d9b19491b5a01fcff6819d28ff28
Erlesand <Erlesand@users.noreply.github.com>
2020-10-04 12:55:37 +0200

    Set up initial HTML structure

--- src/App.js
+++ src/App.js
@@ -5,20 +5,53 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="todo-app">
+        <header className="todo-header">
+          <h1>Todo</h1>
+
+          <input type="text" name="new-item" />
+        </header>
+
+        <ul id="items">
+          <li className="todo-item">
+            <div className="todo-item-view">
+              <input type="checkbox" className="todo-item-toggle" />
+              <input
+                className="todo-item-edit"
+                value="Dip your toes in the JavaScript-water"
+              />
+              <label></label>
+              <button className="todo-item-destroy">X</button>
+            </div>
+          </li>
+
+          <li className="todo-item">
+            <div className="todo-item-view">
+              <input type="checkbox" className="todo-item-toggle" />
+
+              <input
+                className="todo-item-edit"
+                value="Build an awesome todo-app"
+              />
+              <label></label>
+              <button className="todo-item-destroy">X</button>
+            </div>
+          </li>
+        </ul>
+
+        <div>
+          <div>2 items left</div>
+          <div>
+            <input type="radio" name="filter" /> All
+            <input type="radio" name="filter" /> Active
+            <input type="radio" name="filter" /> Completed
+          </div>
+
+          <div>
+            <button>Clear completed</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
